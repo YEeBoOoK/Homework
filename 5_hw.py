f631609b66ce67e259a4933532ec42ebe13ebce3
Elena <elenaosmanova2003@gmail.com>
2025-07-28 12:19:32 +0300

    Добавлена домашка №5

--- 5_hw.py
+++ 5_hw.py
@@ -45,9 +45,9 @@
 # Второе решение
 # Вывод "Элементы не найдены" вместо ошибок
 
-from selenium.common import NoSuchElementException
-from selenium.webdriver.common.by import By
-from selenium import webdriver
+# from selenium.common import NoSuchElementException
+# from selenium.webdriver.common.by import By
+# from selenium import webdriver
 
 # def search_loginform_elements():
 #     driver = webdriver.Chrome()
